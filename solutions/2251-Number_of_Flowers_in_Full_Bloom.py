8fb8d65467e1836847beef5530a921b29cdf51d6
Matěj Hošek <matthy.hosek@gmail.com>
2023-10-17 10:33:49 +0200

    Optimise solution for problem 2251

--- solutions/2251-Number_of_Flowers_in_Full_Bloom.py
+++ solutions/2251-Number_of_Flowers_in_Full_Bloom.py
@@ -1,22 +1,15 @@
 class Solution:
     def fullBloomFlowers(self, flowers: list[list[int]], people: list[int]) -> list[int]:
-        events = []
+        starts = []
+        ends = []
 
         for flower in flowers:
-            events.append((flower[0], -1))
-            events.append((flower[1], 1))
+            starts.append(flower[0])
+            ends.append(flower[1])
 
-        for i in range(len(people)):
-            events.append((people[i], 0, i))
+        starts.sort()
+        ends.sort()
 
-        events.sort()
-        bloomingFlowers = 0
-        for event in events:
-            if len(event) == 2:
-                bloomingFlowers -= event[1]
-                continue
-
-            people[event[2]] = bloomingFlowers
-        return people
+        return [bisect_right(starts, visit) - bisect_left(ends, visit) for visit in people]
 
 print(Solution().fullBloomFlowers(flowers = [[1,10],[3,3]], people = [3,3,2]))
