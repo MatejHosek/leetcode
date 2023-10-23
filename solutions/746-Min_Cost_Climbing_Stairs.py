a4a38dd18b49c889097ddd8f73bfbd071c476b3a
Matěj Hošek <matthy.hosek@gmail.com>
2023-10-23 20:28:44 +0200

    Improve solution 746

--- solutions/746-Min_Cost_Climbing_Stairs.py
+++ solutions/746-Min_Cost_Climbing_Stairs.py
@@ -2,8 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
         stairCosts = [0, 0] + [float('inf')] * len(cost)
 
-        for stair in range(len(cost)):
-            stairCosts[stair + 1] = min(stairCosts[stair] + cost[stair], stairCosts[stair + 1])
-            stairCosts[stair + 2] = min(stairCosts[stair] + cost[stair], stairCosts[stair + 2])
+        for stair in range(2, len(cost) + 1):
+            stairCosts[stair] = min(stairCosts[stair - 2] + cost[stair - 2], stairCosts[stair - 1] + cost[stair - 1])
 
         return stairCosts[len(cost)]
