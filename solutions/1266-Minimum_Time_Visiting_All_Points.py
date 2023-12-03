5f865ef8b81506f091b6db3c83f6686c601864b2
Matěj Hošek <matthy.hosek@gmail.com>
2023-12-03 11:31:12 +0100

    Improve solution 1266

--- solutions/1266-Minimum_Time_Visiting_All_Points.py
+++ solutions/1266-Minimum_Time_Visiting_All_Points.py
@@ -1,3 +1,8 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: list[list[int]]) -> int:
-        return sum([ abs(points[x][0] - points[x + 1][0]) + abs(points[x][1] - points[x + 1][1]) - min(abs(points[x][0] - points[x + 1][0]), abs(points[x][1] - points[x + 1][1])) for x in range(len(points) - 1)])
+        distance = 0
+
+        for x in range(len(points) - 1):
+            distance += abs(points[x][0] - points[x + 1][0]) + abs(points[x][1] - points[x + 1][1]) - min(abs(points[x][0] - points[x + 1][0]), abs(points[x][1] - points[x + 1][1]))
+
+        return distance
